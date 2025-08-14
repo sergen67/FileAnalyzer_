c4854a778c81f8b7a4d589e84e92b7367b3a95e9
sergenilhany <sergen.yagli@ogr.dpu.edu.tr>
2025-08-14 09:58:44 +0300

    Add project files.

---
+++ FileAnalyzer_/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FileAnalyzer_
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Logger.Init();
+
+        }
+    }
+}
